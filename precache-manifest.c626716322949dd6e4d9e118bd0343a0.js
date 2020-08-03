fd03d9de21f92187bac3d8bbcc1995a8e87532d5
morteza <morset65@gamil.com>
2020-08-04 01:34:29 +0430

    Updates

---
+++ precache-manifest.c626716322949dd6e4d9e118bd0343a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48e1f03aecdff654d66cab7fad05088",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "5d7b6e27a87dfe872953",
+    "url": "/react-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/react-website/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-website/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7b6e27a87dfe872953",
+    "url": "/react-website/static/js/main.06a57b20.chunk.js"
+  },
+  {
+    "revision": "6a49467c11d17f451514",
+    "url": "/react-website/static/js/runtime-main.e5bca042.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
